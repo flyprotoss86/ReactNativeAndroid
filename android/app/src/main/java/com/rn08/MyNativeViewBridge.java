6f6d1c4a82eb5e2090aa25405a2463ce8b995b05
flyprotoss86 <flyprotoss@gmail.com>
2018-08-22 19:10:19 +0800

    rn 声网 基础版 init

---
+++ android/app/src/main/java/com/rn08/MyNativeViewBridge.java
@@ -0,0 +1,28 @@
+package com.rn08;
+import android.content.Intent;
+import android.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class MyNativeViewBridge extends ReactContextBaseJavaModule {
+    ReactApplicationContext aContext;
+    public MyNativeViewBridge(ReactApplicationContext reactContext){
+        super(reactContext);
+        aContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "MyNativeViewBridge";
+    }
+
+    @ReactMethod
+    public void HandleCallView(String aMessage){
+        Log.i("RNmessage", "receive from RN: "+ aMessage);
+        Intent aIntent = new Intent(aContext, VideoChatViewActivity.class);
+        aIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        aContext.startActivity(aIntent);
+    }
+}
